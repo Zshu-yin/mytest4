ea69fdbc2fd3e6df944dbef1143a2832baf7d83f
Zsyin <2426151653@qq.com>
2020-09-01 16:14:54 +0800

    冲突测试_2

--- src/main/java/com/zsy/pojo/User.java
+++ src/main/java/com/zsy/pojo/User.java
@@ -1,8 +1,11 @@
 package com.zsy.pojo;
 
+import java.util.jar.JarEntry;
+
 public class User {
     private String name;
     private int age;
     private char gender;
     private String like;
+    private JarEntry jarEntry;
 }

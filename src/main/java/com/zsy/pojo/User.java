6cb37fa6099177e29f660c8d62c0210577472139
Zsyin <2426151653@qq.com>
2020-09-01 16:13:26 +0800

    冲突测试_1

--- src/main/java/com/zsy/pojo/User.java
+++ src/main/java/com/zsy/pojo/User.java
@@ -1,8 +1,11 @@
 package com.zsy.pojo;
 
+import java.util.HashMap;
+
 public class User {
     private String name;
     private int age;
     private char gender;
     private String like;
+    private HashMap hashMap;
 }

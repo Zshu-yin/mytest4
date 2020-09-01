2a01b938f29b12458f9976e367cf31893707f8e8
Zsyin <2426151653@qq.com>
2020-09-01 16:10:42 +0800

    冲突测试_1

--- src/main/java/com/zsy/pojo/User.java
+++ src/main/java/com/zsy/pojo/User.java
@@ -1,7 +1,10 @@
 package com.zsy.pojo;
 
+import java.util.GregorianCalendar;
+
 public class User {
     private String name;
     private int age;
     private char gender;
+    private GregorianCalendar gregorianCalendar;
 }
